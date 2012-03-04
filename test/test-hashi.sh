b63038f6dfeec6af1c753b66b88e25106206841a
Erich Smith <heartquest@gmail.com>
2012-03-03 23:13:51 -0500

    refactor tests

--- test/test-hashi.sh
+++ test/test-hashi.sh
@@ -10,53 +10,44 @@ main() {
     setup $REV
     source $SCRIPT
 
-    echo "running tests for hashi.sh..."
+    echo 'running tests for hashi.sh...'
 
-    label "hsh_store accepts alpha"
+    label 'hsh_store accepts alpha'
     hsh_store topic bucket key val
-    [ "$?" == "0" ] || flunk "Didn't store alpha!"
-    pass
+    assert [ $? == 0 ] || flunk 'Failed to store alpha!'
 
-    label "hsh_store accepts numeric"
+    label 'hsh_store accepts numeric'
     hsh_store 1 2 3 4
-    [ "$?" == "0" ] || flunk "Didn't store numeric!"
-    pass
+    assert [ $? == 0 ] || flunk 'Failed to store numeric!'
 
-    label "hsh_store accepts alphanum with hypens"
+    label 'hsh_store accepts alphanum with hypens'
     hsh_store topic-1 bucket-2 key-3 value-4
-    [ "$?" == "0" ] || flunk "Didn't store numeric!"
-    pass
+    assert [ $? == 0 ] || flunk 'Failed to store numeric!'
 
-    label "hsh_store accepts long value"
-    hsh_store ltopic lbucket lkey "long value here"
-    [ "$?" == "0" ] || flunk "Didn't store long value!"
-    pass
+    label 'hsh_store accepts long value'
+    hsh_store ltopic lbucket lkey 'long value here'
+    assert [ $? == 0 ] || flunk 'Failed to store long value!'
 
     local val
-    label "hsh_get retrieves alpha"
+    label 'hsh_get retrieves alpha'
     val=$(hsh_get topic bucket key)
-    [ "$val" == "val" ] || flunk "Didn't get alpha!"
-    pass
+    assert [ $val == val ] || flunk 'Failed to get alpha!'
 
-    label "hsh_get retrieves numeric"
+    label 'hsh_get retrieves numeric'
     val=$(hsh_get 1 2 3)
-    [ "$val" == "4" ] || flunk "Didn't get numeric!"
-    pass
+    assert [ $val == 4 ] || flunk 'Failed to get numeric!'
 
-    label "hsh_get retrieves alphanum with hypens"
+    label 'hsh_get retrieves alphanum with hypens'
     val=$(hsh_get topic-1 bucket-2 key-3)
-    [ "$val" == "value-4" ] || flunk "Didn't get alphanum with hypens!"
-    pass
+    assert [ $val == value-4 ] || flunk 'Failed to get alphanum with hypens!'
 
-    label "hsh_get retrieves long value"
+    label 'hsh_get retrieves long value'
     val=$(hsh_get ltopic lbucket lkey)
-    [ "$val" == "long value here" ] || flunk "Didn't get long value!"
-    pass
+    assert [ \'$val\' == \'long value here\' ] || flunk 'Failed to get long value!'
 
-    label "hsh_get fails to retrieve unknown key"
+    label 'hsh_get fails to retrieve unknown key'
     val=$(hsh_get some unknown key)
-    [ "$val" == "" ] || flunk "Expected NULL, got: $val"
-    pass
+    assert [ \'$val\' == \'\' ] || flunk 'Expected NULL, got: $val'
 
     teardown
 }
