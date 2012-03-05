9f2b8b2edd45f3e88a566804cd7237279cbe199d
Erich Smith <heartquest@gmail.com>
2012-03-05 16:34:19 -0500

    refactor: flatten hash api, rework get/set tests

--- test/test-hashi.sh
+++ test/test-hashi.sh
@@ -12,32 +12,32 @@ main() {
 
     echo 'running tests for hashi.sh...'
 
-    label stores alpha
-    assert hsh_store topic bucket key val
+    label sets alpha
+    assert hsh_set hash key val
 
-    label stores numeric
-    assert hsh_store 1 2 3 4
+    label sets numeric
+    assert hsh_set hash 2 3
 
-    label stores alphanum with hypens
-    assert hsh_store topic-1 bucket-2 key-3 value-4
+    label sets alphanum with hypens
+    assert hsh_set hash key-2 value-2
 
-    label stores long value
-    assert hsh_store ltopic lbucket lkey \'long value here\'
+    label sets long value
+    assert hsh_set hash lkey \'long value here\'
 
     label retrieves alpha
-    assert [ $(hsh_get topic bucket key) == val ]
+    assert [ $(hsh_get hash key) == val ]
 
     label retrieves numeric
-    assert [ $(hsh_get 1 2 3) == 4 ]
+    assert [ $(hsh_get 2) == 3 ]
 
     label retrieves alphanum with hypens
-    assert [ $(hsh_get topic-1 bucket-2 key-3) == value-4 ]
+    assert [ $(hsh_get key-2) == value-2 ]
 
     label retrieves long value
-    assert [ \'$(hsh_get ltopic lbucket lkey)\' == \'long value here\' ]  # NB: escape '
+    assert [ \'$(hsh_get lkey)\' == \'long value here\' ]  # NB: escape '
 
     label fails to retrieve unknown key
-    assert [ \'$(hsh_get some unknown key)\' == \'\' ] # NB: escape '
+    assert [ \'$(hsh_get unknown key)\' == \'\' ] # NB: escape '
 
     teardown
 }
