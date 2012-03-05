ba495b84c1ff352bb6009895aa6a0b4fd2bba10a
Erich Smith <heartquest@gmail.com>
2012-03-05 09:26:05 -0500

    moved runtests ..

---
+++ runtests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+(
+    cd test
+    source ./init.inc
+
+    start_clock
+
+    for test in $(ls test-*.sh);
+    do
+        ./$test $1
+    done
+
+    echo "All tests complete."
+    stop_clock
+)
+
+exit 0
