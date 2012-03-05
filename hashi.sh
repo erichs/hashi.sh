dc49d24f6da26b21bab151b32f8b9ce78569b3b1
Erich Smith <heartquest@gmail.com>
2012-03-05 16:37:38 -0500

    simplify: flatten hierarchy

--- hashi.sh
+++ hashi.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
-# hashi.sh - ultra-simple hierarchical key/value hash
-#   topics set the 'namespace' for hash-buckets
-#   buckets contain key/value pairs
+# hashi.sh - ultra-simple key/value hash
 
 set -e
 
-hsh_store()
-{
-    local topic=$1 bucket=$2 key=$3 val=$4
-    local fullkey=$(hsh_escape_key "__${topic}_${bucket}_${key}")
+hsh_escape_key() { local key=$1
+    echo ${key//-/___}  # bash doesn't allow hyphens in variable names. bummer.
+}
+
+hsh_set() { local hash=$1 key=$2 val=$3
+    local fullkey=$(hsh_escape_key "__${hash}_${key}")
     eval "$fullkey='$val'"
 }
 
-hsh_get()
-{
-    hsh_save_options && set +u
-    local topic=$1 bucket=$2 key=$3
-    local fullkey=$(hsh_escape_key "__${topic}_${bucket}_${key}")
-    echo ${!fullkey}
-    hsh_restore_options
+hsh_get() { local hash=$1 key=$2
+    local fullkey=$(hsh_escape_key "__${hash}_${key}")
+    echo ${!fullkey:-}  # the {foo:-} idiom is safe to use with set -o nounset | set -u
 }
 
-hsh_escape_key() {  # bash doesn't allow hypens in variable names. bummer.
-    local key=$1
-    echo ${key//-/___}
-}
 
-hsh_save_options() {
-    HSH_SETOPTIONS=$(set +o)
-}
-
-hsh_restore_options() {
-    # leading space in command disables bash history
-    while read opt; do eval " $opt"; done < <(echo "$HSH_SETOPTIONS")
-}
-
-HSH_SETOPTIONS=""
