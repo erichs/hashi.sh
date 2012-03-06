997b52348767293e646dac61aa93fa041331927e
Erich Smith <heartquest@gmail.com>
2012-03-05 20:30:35 -0500

    first pass: escape hash input

--- hashi.sh
+++ hashi.sh
@@ -3,22 +3,22 @@
 
 set -e
 
-hsh_escape_key() { local key=$1
-    echo ${key//-/___}  # bash doesn't allow hyphens in variable names. bummer.
+hsh_escape() { local str=$1
+    echo ${str//-/___}  # bash doesn't allow hyphens in variable names. bummer.
 }
 
 hsh_set() { local hash=$1 key=$2 val=$3
-    local fullkey=$(hsh_escape_key "__${hash}_${key}")
+    local fullkey=$(hsh_escape "__${hash}_${key}")
     eval "$fullkey='$val'"
 }
 
 hsh_get() { local hash=$1 key=$2
-    local fullkey=$(hsh_escape_key "__${hash}_${key}")
+    local fullkey=$(hsh_escape "__${hash}_${key}")
     echo ${!fullkey:-}  # the {foo:-} idiom is safe to use with set -o nounset | set -u
 }
 
 hsh_keys() { local hash=$1
-    local prefix="__${hash}_"
+    local prefix=$(hsh_escape "__${hash}_")
     local vars
     eval vars="\${!$prefix*}"
     for var in $vars; do
