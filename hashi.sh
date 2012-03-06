dfbb902aed02003165213fc90dc3e462197033dc
Erich Smith <heartquest@gmail.com>
2012-03-06 14:12:45 -0500

    GREEN: rm removes key

--- hashi.sh
+++ hashi.sh
@@ -3,22 +3,20 @@
 
 set -e
 
-hsh_escape() { local str=$1
-    echo ${str//-/___}  # bash doesn't allow hyphens in variable names. bummer.
-}
+#### API: use these primitives in your script
 
 hsh_set() { local hash=$1 key=$2 val=$3
-    local fullkey=$(hsh_escape "__${hash}_SNIP_${key}")
+    local fullkey=$(hsh_generate_key $hash $key)
     eval "$fullkey='$val'"
 }
 
 hsh_get() { local hash=$1 key=$2
-    local fullkey=$(hsh_escape "__${hash}_SNIP_${key}")
+    local fullkey=$(hsh_generate_key $hash $key)
     echo ${!fullkey:-}  # the {foo:-} idiom is safe to use with set -o nounset | set -u
 }
 
 hsh_keys() { local hash=$1
-    local prefix=$(hsh_escape "__${hash}_SNIP_")
+    local prefix=$(hsh_generate_key $hash)
     local vars
     eval vars="\${!$prefix*}"
     for var in $vars; do
@@ -39,7 +37,18 @@ hsh_rm() { local hash=$1 key=${2:-}
     fi
 }
 
+#### internal helper methods
+
 hsh_unset_key() { local hash=$1 key=$2
     local fullkey=$(hsh_escape "__${hash}_SNIP_${key}")
     unset -v $fullkey
 }
+
+hsh_generate_key() { local hash=$1 key=${2:-}
+    hsh_escape "__${hash}_SNIP_${key}"
+}
+
+hsh_escape() { local str=$1
+    echo ${str//-/___}  # bash doesn't allow hyphens in variable names. bummer.
+}
+
