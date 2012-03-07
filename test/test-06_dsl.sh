aae50503518367fc036117ac448ca304f022f639
Erich Smith <heartquest@gmail.com>
2012-03-07 15:14:22 -0500

    RED: hsh set hash is error

--- test/test-06_dsl.sh
+++ test/test-06_dsl.sh
@@ -28,9 +28,15 @@ main() {
     label hsh with one parameter gives usage
     assert [ $(hsh parm1 | grep "Usage" | wc -l) == 1 ]
 
-    label hsh with get hash key retrieves value
+    label 'hsh get hash' is error
+    assert [ $(hsh get hash | grep "must provide key" | wc -l) == 1 ]
+
+    label 'hsh get hash key' retrieves value
     hsh_set hash key val
     assert [ $(hsh get hash key | grep "val") == val ]
+
+    label 'hsh set hash' is error
+    assert [ $(hsh set hash | grep "must provide key" | wc -l) == 1 ]
 }
 
 main
