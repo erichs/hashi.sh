6c74cb8d3fd9e184337f37eb90b7a75041cbceb8
Erich Smith <heartquest@gmail.com>
2012-03-06 21:33:44 -0500

    RED: each iterates over all keys

---
+++ test/test-04_each.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -u
+
+source init.inc
+
+trap teardown EXIT # guarantee fixture teardown
+
+REV=${1:-}
+SCRIPT='./hashi.sh'
+
+main() {
+    setup $REV
+    source $SCRIPT
+
+    echo 'EACH METHOD...'
+
+    hsh_set hash key1 val1
+    hsh_set hash key2 val3
+    hsh_set hash key3 val3
+
+    label each iterates over all keys
+    assert [ $(hsh_each hash \'echo foo\' | wc -l) == 3 ]
+
+}
+
+main
