ea1778031879283a948940f23da9cb0617952f26
Erich Smith <heartquest@gmail.com>
2012-03-07 15:47:37 -0500

    GREEN: hsh set hash is error

--- extras/hsh-dsl.sh
+++ extras/hsh-dsl.sh
@@ -5,12 +5,10 @@ hsh() { local op=${1:-} hash=${2:-} key=${3:-}
         echo "Usage: op hsh some usage here"
     fi
     case $op in
-        get)    if [ -z "$key" ]; then
-                    echo "must provide key with $op operation!"
-                    exit 1
-                else
-                    hsh_get $hash $key
-                fi;;
+        get)        hsh_enforce_parameter key
+                    hsh_get $hash $key;;
+        set)        hsh_enforce_parameter key
+                    :;;
     esac
 }
 
