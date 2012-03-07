6f6dcdce30de46c5cae132632c0b9f7c6c995df9
Erich Smith <heartquest@gmail.com>
2012-03-07 14:31:19 -0500

    GREEN: hsh get hash key

--- extras/hsh-dsl.sh
+++ extras/hsh-dsl.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-hsh() { local op=${1:-} hash=${2:-}
+hsh() { local op=${1:-} hash=${2:-} key=${3:-}
     if [ -z "$hash" ]; then
         echo "Usage: op hsh some usage here"
     fi
+    case $op in
+        get)    if [ -z "$key" ]; then
+                    echo "must provide key with $op operation!"
+                    exit 1
+                else
+                    hsh_get $hash $key
+                fi;;
+    esac
 }
+
+
