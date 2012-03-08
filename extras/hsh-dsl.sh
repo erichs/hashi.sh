b76576b5fea62e926a26e1c1e60bbcb4929a7bb6
Erich Smith <heartquest@gmail.com>
2012-03-08 17:20:57 -0500

    dsl: attempt removal of attempt_ handlers

--- extras/hsh-dsl.sh
+++ extras/hsh-dsl.sh
@@ -7,7 +7,7 @@ hsh() { local op=${1:-} hash=${2:-} key=${3:-} value=${4:-}
     fi
     case $op in
         get)        hsh_check_arg key   || return 1
-                    hsh_attempt_get $hash $key;;
+                    hsh_get $hash $key;;
         set)        hsh_check_arg key   || return 1
                     hsh_check_arg value || return 1
                     hsh_set $hash $key $value;;
