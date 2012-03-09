e51e86b3436d01835b424e39127a48520cd37512
Erich Smith <heartquest@gmail.com>
2012-03-08 21:02:00 -0500

    refactor

--- hashi.sh
+++ hashi.sh
@@ -2,6 +2,15 @@
 # hashi.sh - ultra-simple key/value hash
 
 #### API: use these primitives in your script
+# get:    2 parameters, retrieves the value for a key
+# set:    3 parameters, sets a key/value pair
+# del:    1 or 2 parameters, deletes hash or hash key
+# has:    2 parameters, true/false: does hash contain key?
+# keys:   1 parameter, displays all hash keys
+# values: 1 parameter, displays all hash values
+# getall: 1 parameter, displays each key/value pair
+# size:   1 parameter, numeric size of all keys
+# each:   2 parameters, iterates over hash, evaluating code
 
 hsh_set() { local hash=$1 key=$2 val=$3
     local fullkey=$(hsh_generate_key $hash $key)
@@ -51,11 +60,15 @@ hsh_each() { local hash=$1 code=$2
 }
 
 hsh_values() { local hash=$1
-   hsh_each $hash 'echo $value'
+    hsh_each $hash 'echo $value'
 }
 
 hsh_getall() { local hash=$1
     hsh_each $hash 'echo $key: $value'
+}
+
+hsh_has() { local hash=$1 key=$2
+    return $(hsh_get $hash $key >/dev/null)
 }
 
 #### internal helper methods
@@ -72,10 +85,14 @@ hsh_unset_hash() { local hash=$1
     for key in $(hsh_keys $hash); do
         hsh_unset_key $hash $key
     done
+    [ $(hsh_size $hash) -gt 0 ] && return 1
+    return 0
 }
 
 hsh_unset_key() { local hash=$1 key=$2
     local fullkey=$(hsh_generate_key $hash $key)
     unset -v $fullkey
+    hsh_has $hash $key && return 1
+    return 0
 }
 
