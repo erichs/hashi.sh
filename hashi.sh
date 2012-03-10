569d05b54a803968c72f5a42305d658c4a08aba5
Erich Smith <heartquest@gmail.com>
2012-03-09 22:44:27 -0500

    remove extras/ dir and fold simplified 'dsl' wrapper into hashi.sh

--- hashi.sh
+++ hashi.sh
@@ -4,6 +4,22 @@
 #### API: use these primitives in your script
 # See apidoc.md for full documentation
 
+hsh() { local op=${1:-}
+    if [ -z "$op" ]; then
+        __usage
+        return 1
+    fi
+    for method in $(__methods); do
+        if [ "hsh_$op" == $method ]; then
+            shift
+            $method $*
+            return $?
+        fi
+    done
+    __usage
+    return 1
+}
+
 hsh_set() { local hash=${1:-} key=${2:-} value=${3:-}
     optional_doc <<-'end' && return 0
 	### hsh_set
@@ -244,6 +260,27 @@ hsh_empty() { local hash=${1:-}
     return 0
 }
 
+hsh_declare() { local hash=$1
+    optional_doc <<-'end' && return 0
+	### hsh_declare
+
+	Declare a wrapper function around 'hsh()'. This allows you to eliminate one argument
+	from all subsequent api calls for that hash.
+
+	Required parameters: hash
+
+	```bash
+	$ hsh_declare dogs
+	$ dogs set breed Collie
+	$ dogs get breed
+	Collie
+	$
+	```
+	end
+    __check_args hash
+    echo "hash is: $hash"
+    eval "$hash() { op=\$1; shift; hsh \$op $hash \$*; }"
+}
 #### internal helper methods
 
 __generate_key() { local hash=${1:-} key=${2:-}
@@ -298,4 +335,19 @@ __generate_api() {
     __display_documentation=1
     for method in $(__methods); do eval $method; echo ; echo ;  done > apidoc.md
     unset __display_documentation
+}
+
+__usage() {
+    echo "Usage: $(__outermost_function) op hashname [key] [value]"
+    echo "   where op is one of:"
+    local prefix="hsh_"
+    for method in $(__methods); do
+        echo "   ${method#$prefix}"  # auto-generate op list
+    done
+}
+
+__outermost_function() {
+    # returns name of 'outermost' function: last element of FUNCNAME[].
+    local funclist=${FUNCNAME[@]}    # space-delimited list of function callers, arbitrary depth
+    echo ${funclist##* }             # eat everything up through the last space
 }
--- test/test-06_hsh.sh
+++ test/test-06_hsh.sh
@@ -9,14 +9,12 @@ trap teardown EXIT # guarantee fixture teardown
 
 REV=${1:-}
 SCRIPT='./hashi.sh'
-EXTENSION='./extras/hsh-dsl.sh'
 
 main() {
     setup $REV
     source $SCRIPT
-    source $EXTENSION
 
-    discreet_echo 'DSL EXTENSION...\n'
+    discreet_echo 'HSH Wrapper...\n'
 
     set +e
 
