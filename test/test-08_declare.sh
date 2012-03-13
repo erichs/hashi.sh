ed08f2b82cd326c1b4935a7873a00f678744febf
Erich Smith <heartquest@gmail.com>
2012-03-12 22:56:16 -0400

    hsh_undeclare now removes wrapper function

--- test/test-08_declare.sh
+++ test/test-08_declare.sh
@@ -14,78 +14,85 @@ main() {
     setup $REV
     source $SCRIPT
 
-    discreet_echo 'test Wrapper...\n'
+    discreet_echo 'DECLARE AND UNDECLARE...\n'
 
     set +e
 
     label hsh_declare is implemented
-    hsh_declare test >/dev/null
+    hsh_declare hshtest >/dev/null
     assert [ $? == 0 ]
 
-    label test without parameters gives usage
-    assert [ $(test | greplines "Usage") == 1 ]
+    label hshtest without parameters gives usage
+    assert [ $(hshtest | greplines "Usage") == 1 ]
 
-    label test with one parameter gives usage
-    assert [ $(test parm1 | greplines "Usage") == 1 ]
+    label hshtest with one parameter gives usage
+    assert [ $(hshtest parm1 | greplines "Usage") == 1 ]
 
-    label test with unknown op gives usage
-    assert [ $(test unknown_op | greplines "Usage") == 1 ]
+    label hshtest with unknown op gives usage
+    assert [ $(hshtest unknown_op | greplines "Usage") == 1 ]
 
     for method in $(__hsh_dsl_methods); do
         op=${method#hsh_}
         label usage contains $op
-        assert [ $(test | greplines "$op") -ge 1 ]
+        assert [ $(hshtest | greplines "$op") -ge 1 ]
     done
 
-    label 'test get' is error
-    assert [ $(test get | greplines "must provide key") == 1 ]
+    label 'hshtest get' is error
+    assert [ $(hshtest get | greplines "must provide key") == 1 ]
 
-    label 'test get boguskey' gives error code
-    test get boguskey >/dev/null 2>&1
+    label 'hshtest get boguskey' gives error code
+    hshtest get boguskey >/dev/null 2>&1
     assert [ $? == 1 ]
 
-    label 'test del key1' does not return error
-    test set key1 val1
-    test del key1 >/dev/null 2>&1
+    label 'hshtest del key1' does not return error
+    hshtest set key1 val1
+    hshtest del key1 >/dev/null 2>&1
     assert [ $? == 0 ]
 
-    label 'test del' does return error
-    test del >/dev/null 2>&1
+    label 'hshtest del' does return error
+    hshtest del >/dev/null 2>&1
     assert [ $? == 1 ]
 
     set -e
 
-    label 'test get key' retrieves value
-    test set key val
-    assert [ $(test get key | grep "val") == val ]
+    label 'hshtest get key' retrieves value
+    hshtest set key val
+    assert [ $(hshtest get key | grep "val") == val ]
 
-    label 'test set' is error
-    assert [ $(test set | greplines "must provide key") == 1 ]
+    label 'hshtest set' is error
+    assert [ $(hshtest set | greplines "must provide key") == 1 ]
 
-    label 'test set key' is error
-    assert [ $(test set key | greplines "must provide value") == 1 ]
+    label 'hshtest set key' is error
+    assert [ $(hshtest set key | greplines "must provide value") == 1 ]
 
-    label 'test set key2 val2' gets value
-    test set key2 val2
-    assert [ $(test get key2) == val2 ]
+    label 'hshtest set key2 val2' gets value
+    hshtest set key2 val2
+    assert [ $(hshtest get key2) == val2 ]
 
-    label 'test get boguskey' gives empty string
-    assert [ $(test get boguskey) == '' ]
+    label 'hshtest get boguskey' gives empty string
+    assert [ $(hshtest get boguskey) == '' ]
 
     set +e
 
-    label 'test keys' does not return error
-    test keys >/dev/null 2>&1
+    label 'hshtest keys' does not return error
+    hshtest keys >/dev/null 2>&1
     assert [ $? == 0 ]
 
-    label 'test size' does not return error
-    test size >/dev/null 2>&1
+    label 'hshtest size' does not return error
+    hshtest size >/dev/null 2>&1
     assert [ $? == 0 ]
 
     set -e
 
-    label 'test size' gives correct size
-    assert [ $(test size) == 2 ]
+    label 'hshtest size' gives correct size
+    assert [ $(hshtest size) == 2 ]
+
+    label hsh_undeclare is implemented
+    hsh_undeclare hshtest >/dev/null
+    assert [ $? == 0 ]
+
+    label hshtest is undeclared
+    assert [ $(hshtest 2>&1 | greplines "command not found") == 1 ]
 
 }
 
